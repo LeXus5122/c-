9ccb63d0a7f7ac8527bbd665dd232842b5f2c020
LeXus22 <lexus22@inbox.ru>
2023-03-01 01:32:23 +0300

    example010

---
+++ Example010_methodarray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 3, 54, 56, 7, 8, 54, };
+
+int n = array.Length;
+int find = 54;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        //break;
+    }
+    index++;
+}

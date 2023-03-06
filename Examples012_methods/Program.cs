716f81916b24c4d647af1d5186a068ce7d8758db
LeXus22 <lexus22@inbox.ru>
2023-03-06 20:55:11 +0300

    012repl

--- Examples012_methods/Program.cs
+++ Examples012_methods/Program.cs
@@ -56,8 +56,8 @@ int year = Method3();
 string Method4(int count, string c)
 {
     string result = String.Empty;
- for (int i=0; i<count; i++)
-        {
+    for (int i = 0; i < count; i++)
+    {
         result = result + c;
     }
     return result;
@@ -66,11 +66,44 @@ string Method4(int count, string c)
 string res = Method4(10, "1vfv");
 //Console.WriteLine(res);
 
-for (int i=2; i<=10; i++)
+// for (int i=2; i<=10; i++)
+// {
+//     for (int j=2; j<=10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
++ "ежели бы вас послали вместо нашего милого Винценгероде,"
++ " вы бы взяли приступом согласие прусского короля. "
++ "Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwerty"
+//              012
+// s[3] //r
+
+string Replace(string text, char oldValue, char newValue)
 {
-    for (int j=2; j<=10; j++)
+    string result = String.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
     {
-        Console.WriteLine($"{i} x {j} = {i*j}");
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
-    Console.WriteLine();
+    return result;
 }
+
+string newText = Replace(text, ' ', '-');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'с', 'С');
+Console.WriteLine(newText);
